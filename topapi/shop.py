cfb1fc50445892b11e33f2b0167d88ba92ab34d2
wgd243@gmail.com <wgd243@gmail.com@77b0f9dd-2c0a-c265-1d67-5e706b75b7b5>
2011-07-16 07:53:21 +0000

    fjals

--- topapi/shop.py
+++ topapi/shop.py
@@ -7,11 +7,16 @@ class Shop(TBase):
     method = 'shop.'
     fields='sid,cid,nick,title,desc,bulletin,pic_path,created,modified,shop_score,remain_count,all_count,used_count'
 
-    def get(self, sessionkey, nick, fields=''):
+    def get(self, nick, fields=''):
         self.method = 'shop.get'
-        self.session = sessionkey
+        self.session ='' 
         self.setParams(nick=nick)
         if fields:
             self.fields=fields
         return self.do() 
 
+
+if __name__ == '__main__':
+    s = Shop()
+
+    print s.get('淘金电商', fields='sid')
