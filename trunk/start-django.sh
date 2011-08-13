e2b96c4be4847a68225c313463684742a85fd27c
wgd243@gmail.com <wgd243@gmail.com@77b0f9dd-2c0a-c265-1d67-5e706b75b7b5>
2011-08-13 07:07:48 +0000

    fjal

--- trunk/start-django.sh
+++ trunk/start-django.sh
@@ -1,21 +1,17 @@
 #!/bin/bash 
-set -x 
-rootdir=/home/wgwang/develop/pytop 
-cd $rootdir
-kill -9 `cat /home/wgwang/develop/pytop/django_pid`
+set -x  
+shdir=${0%/*}
+cd $shdir
+rootdir=$PWD
+echo $rootdir
+kill -9 `cat $rootdir/django_pid`
 rm -f $rootdir/django_pid
-export DJANGO_SETTINGS_MODULE=top.settings
+export DJANGO_SETTINGS_MODULE=newtop.settings
 #python ./django-admin.py runfcgi --pythonpath=$rootdir  protocol=scgi host=127.0.0.1 port=8101 daemonize=true method=prefork maxrequests=100000 maxchildren=30 pidfile=$rootdir/django_pid
 python ./django-admin.py runfcgi --pythonpath=$rootdir  protocol=scgi host=127.0.0.1 port=8101 method=prefork maxrequests=100000 maxchildren=30 pidfile=$rootdir/django_pid
 
 sleep 3
 
 ps -ef | grep django 
-
-sleep 1
-
-curl "http://localhost:8100/top/thread/startthread"
-
-
 
 
