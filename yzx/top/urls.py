7992cbe4bf8d716310eb9d568f90bd7756b3073c
wgd243@gmail.com <wgd243@gmail.com@77b0f9dd-2c0a-c265-1d67-5e706b75b7b5>
2011-08-28 05:19:22 +0000

    fix

--- yzx/top/urls.py
+++ yzx/top/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
-from newtop.views import *
+from top.views import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
