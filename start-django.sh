7a38bbe15bd6a6f43a6f28f8968b452d697e4d77
wgd243@gmail.com <wgd243@gmail.com@77b0f9dd-2c0a-c265-1d67-5e706b75b7b5>
2011-08-28 05:35:33 +0000

    delete newtop and top

--- start-django.sh
+++ start-django.sh
@@ -8,9 +8,10 @@ kill  `cat $rootdir/django_pid`
 sleep 2
 rm -f $rootdir/django_pid
 ps -ef | grep django 
-export DJANGO_SETTINGS_MODULE=newtop.settings
+
+export DJANGO_SETTINGS_MODULE=top.settings
 #python ./django-admin.py runfcgi --pythonpath=$rootdir  protocol=scgi host=127.0.0.1 port=8101 daemonize=true method=prefork maxrequests=100000 maxchildren=30 pidfile=$rootdir/django_pid
-python ./django-admin.py runfcgi --pythonpath=$rootdir  protocol=scgi host=127.0.0.1 port=8101 method=prefork maxrequests=100000 maxchildren=30 pidfile=$rootdir/django_pid
+python ./django-admin.py runfcgi --pythonpath=$rootdir --pythonpath=$rootdir/yzx  protocol=scgi host=127.0.0.1 port=8101 method=prefork maxrequests=100000 maxchildren=30 pidfile=$rootdir/django_pid
 
 sleep 2
 
