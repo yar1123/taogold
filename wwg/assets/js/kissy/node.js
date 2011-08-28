2e361d1fb1b35e491d423c17776fb69e0d24c0fe
fool2fish@gmail.com <fool2fish@gmail.com@77b0f9dd-2c0a-c265-1d67-5e706b75b7b5>
2011-08-28 13:35:31 +0000

    add new kissy

---
+++ wwg/assets/js/kissy/node.js
@@ -0,0 +1,623 @@
+﻿/*
+Copyright 2011, KISSY UI Library v1.20dev
+MIT Licensed
+build time: Aug 25 16:20
+*/
+/**
+ * @module  anim-node-plugin
+ * @author  lifesinger@gmail.com,
+ *          qiaohua@taobao.com,
+ *          yiminghe@gmail.com
+ */
+KISSY.add('node/anim-plugin', function(S, DOM, Anim, N, undefined) {
+
+    var NLP = N.prototype,
+        ANIM_KEY = "ksAnims" + S.now(),
+        DISPLAY = 'display',
+        NONE = 'none',
+        OVERFLOW = 'overflow',
+        HIDDEN = 'hidden',
+        OPCACITY = 'opacity',
+        HEIGHT = 'height',
+        SHOW = "show",
+        HIDE = "hide",
+        FADE = "fade",
+        SLIDE = "slide",
+        TOGGLE = "toggle",
+        WIDTH = 'width',
+        FX = {
+            show: [OVERFLOW, OPCACITY, HEIGHT, WIDTH],
+            fade: [OPCACITY],
+            slide: [OVERFLOW, HEIGHT]
+        };
+
+    N.__ANIM_KEY = ANIM_KEY;
+
+    (function(P) {
+
+        function attachAnim(elem, anim) {
+            var anims = DOM.data(elem, ANIM_KEY);
+            if (!anims) {
+                DOM.data(elem, ANIM_KEY, anims = []);
+            }
+            anim.on("complete", function() {
+                var anims = DOM.data(elem, ANIM_KEY);
+                if (anims) {
+                    // 结束后从关联的动画队列中删除当前动画
+                    var index = S.indexOf(anim, anims);
+                    if (index >= 0) {
+                        anims.splice(index, 1);
+                    }
+                    if (!anims.length) {
+                        DOM.removeData(elem, ANIM_KEY);
+                    }
+                }
+            });
+            // 当前节点的所有动画队列
+            anims.push(anim);
+        }
+
+        P.animate = function() {
+            var self = this,
+                args = S.makeArray(arguments);
+            S.each(self, function(elem) {
+                var anim = Anim.apply(undefined, [elem].concat(args)).run();
+                attachAnim(elem, anim);
+            });
+            return self;
+        };
+
+        P.stop = function(finish) {
+            var self = this;
+            S.each(self, function(elem) {
+                var anims = DOM.data(elem, ANIM_KEY);
+                if (anims) {
+                    S.each(anims, function(anim) {
+                        anim.stop(finish);
+                    });
+                    DOM.removeData(elem, ANIM_KEY);
+                }
+            });
+            return self;
+        };
+
+        S.each({
+                show: [SHOW, 1],
+                hide: [SHOW, 0],
+                fadeIn: [FADE, 1],
+                fadeOut: [FADE, 0],
+                slideDown: [SLIDE, 1],
+                slideUp: [SLIDE, 0]
+            },
+            function(v, k) {
+                P[k] = function(speed, callback, easing, nativeSupport) {
+                    var self = this;
+                    // 没有参数时，调用 DOM 中的对应方法
+                    if (DOM[k] && !speed) {
+                        DOM[k](self);
+                    } else {
+                        S.each(self, function(elem) {
+                            var anim = fx(elem, v[0], speed, callback,
+                                v[1], easing || 'easeOut', nativeSupport);
+                            attachAnim(elem, anim);
+                        });
+                    }
+                    return self;
+                };
+            });
+
+        // toggle 提出来单独写，清晰点
+        P[TOGGLE] = function(speed) {
+            var self = this;
+            P[self.css(DISPLAY) === NONE ? SHOW : HIDE].apply(self, arguments);
+        };
+    })(NLP);
+
+    function fx(elem, which, speed, callback, visible, easing, nativeSupport) {
+
+        if (visible) {
+            DOM.show(elem);
+        }
+
+        // 根据不同类型设置初始 css 属性, 并设置动画参数
+        var originalStyle = {}, style = {};
+        S.each(FX[which], function(prop) {
+            /**
+             * 2011-08-19
+             * originalStyle 记录行内样式，防止外联样式干扰！
+             */
+            var elemStyle = elem.style;
+            if (prop === OVERFLOW) {
+                originalStyle[OVERFLOW] = elemStyle[OVERFLOW];
+                DOM.css(elem, OVERFLOW, HIDDEN);
+            }
+            else if (prop === OPCACITY) {
+                // 取行内 opacity
+                originalStyle[OPCACITY] = DOM.style(elem, OPCACITY);
+                style.opacity = visible ? 1 : 0;
+                if (visible) {
+                    DOM.css(elem, OPCACITY, 0);
+                }
+            }
+            else if (prop === HEIGHT) {
+                originalStyle[HEIGHT] = elemStyle[HEIGHT];
+                //http://arunprasad.wordpress.com/2008/08/26/naturalwidth-and-naturalheight-for-image-element-in-internet-explorer/
+                style.height = (visible ?
+                    DOM.height(elem) || elem.naturalHeight :
+                    0) + "px";
+                if (visible) {
+                    DOM.css(elem, HEIGHT, 0);
+                }
+            }
+            else if (prop === WIDTH) {
+                originalStyle[WIDTH] = elemStyle[WIDTH];
+                style.width = (visible ?
+                    DOM.width(elem) || elem.naturalWidth :
+                    0) + "px";
+                if (visible) {
+                    DOM.css(elem, WIDTH, 0);
+                }
+            }
+        });
+
+        // 开始动画
+        return new Anim(elem, style, speed, easing, function() {
+            // 如果是隐藏，需要设置 diaplay
+            if (!visible) {
+                DOM.hide(elem);
+            }
+
+            // 还原样式
+            if (originalStyle[HEIGHT] !== undefined) {
+                DOM.css(elem, "height", originalStyle[HEIGHT]);
+            }
+            if (originalStyle[WIDTH] !== undefined) {
+                DOM.css(elem, "width", originalStyle[WIDTH]);
+            }
+            if (originalStyle[OPCACITY] !== undefined) {
+                DOM.css(elem, "opacity", originalStyle[OPCACITY]);
+            }
+            if (originalStyle[OVERFLOW] !== undefined) {
+                DOM.css(elem, "overflow", originalStyle[OVERFLOW]);
+            }
+
+            if (callback) {
+                callback();
+            }
+
+        }, nativeSupport).run();
+    }
+
+}, {
+    requires:["dom","anim","./base"]
+});
+/**
+ * 2011-05-17
+ *  - 承玉：添加 stop ，随时停止动画
+ *
+ *  TODO
+ *  - anim needs queue mechanism ?
+ */
+/**
+ * import methods from DOM to NodeList.prototype
+ * @author  yiminghe@gmail.com
+ */
+KISSY.add('node/attach', function(S, DOM, Event, NodeList, undefined) {
+
+    var NLP = NodeList.prototype,
+        makeArray = S.makeArray,
+        // DOM 添加到 NP 上的方法
+        // if DOM methods return undefined , Node methods need to transform result to itself
+        DOM_INCLUDES_NORM = [
+            "equals",
+            "contains",
+            "scrollTop",
+            "scrollLeft",
+            "height",
+            "width",
+            "addStyleSheet",
+            // "append" will be overridden
+            "appendTo",
+            // "prepend" will be overridden
+            "prependTo",
+            "insertBefore",
+            "before",
+            "after",
+            "insertAfter",
+            "test",
+            "hasClass",
+            "addClass",
+            "removeClass",
+            "replaceClass",
+            "toggleClass",
+            "removeAttr",
+            "hasAttr",
+            "hasProp",
+            // anim override
+//            "show",
+//            "hide",
+//            "toggle",
+            "scrollIntoView",
+            "remove",
+            "removeData",
+            "hasData",
+            "unselectable"
+        ],
+        // if return array ,need transform to nodelist
+        DOM_INCLUDES_NORM_NODE_LIST = [
+            "filter",
+            "first",
+            "parent",
+            "closest",
+            "next",
+            "prev",
+            "clone",
+            "siblings",
+            "children"
+        ],
+        // if set return this else if get return true value ,no nodelist transform
+        DOM_INCLUDES_NORM_IF = {
+            // dom method : set parameter index
+            "attr":1,
+            "text":0,
+            "css":1,
+            "style":1,
+            "val":0,
+            "prop":1,
+            "offset":0,
+            "html":0,
+            "data":1
+        },
+        // Event 添加到 NP 上的方法
+        EVENT_INCLUDES = ["on","detach","fire","delegate","undelegate"];
+
+
+    function accessNorm(fn, self, args) {
+        args.unshift(self);
+        var ret = DOM[fn].apply(DOM, args);
+        if (ret === undefined) {
+            return self;
+        }
+        return ret;
+    }
+
+    function accessNormList(fn, self, args) {
+        args.unshift(self);
+        var ret = DOM[fn].apply(DOM, args);
+        if (ret === undefined) {
+            return self;
+        }
+        else if (ret === null) {
+            return null;
+        }
+        return new NodeList(ret);
+    }
+
+    function accessNormIf(fn, self, index, args) {
+
+        // get
+        if (args[index] === undefined
+            // 并且第一个参数不是对象，否则可能是批量设置写
+            && !S.isObject(args[0])) {
+            args.unshift(self);
+            return DOM[fn].apply(DOM, args);
+        }
+        // set
+        return accessNorm(fn, self, args);
+    }
+
+    S.each(DOM_INCLUDES_NORM, function(k) {
+        NLP[k] = function() {
+            var args = makeArray(arguments);
+            return accessNorm(k, this, args);
+        };
+    });
+
+    S.each(DOM_INCLUDES_NORM_NODE_LIST, function(k) {
+        NLP[k] = function() {
+            var args = makeArray(arguments);
+            return accessNormList(k, this, args);
+        };
+    });
+
+    S.each(DOM_INCLUDES_NORM_IF, function(index, k) {
+        NLP[k] = function() {
+            var args = makeArray(arguments);
+            return accessNormIf(k, this, index, args);
+        };
+    });
+
+    S.each(EVENT_INCLUDES, function(k) {
+        NLP[k] = function() {
+            var args = makeArray(arguments);
+            args.unshift(this);
+            return Event[k].apply(Event, args);
+        }
+    });
+
+}, {
+    requires:["dom","event","./base"]
+});
+
+/**
+ * 2011-05-24
+ *  - 承玉：
+ *  - 将 DOM 中的方法包装成 NodeList 方法
+ *  - Node 方法调用参数中的 KISSY NodeList 要转换成第一个 HTML Node
+ *  - 要注意链式调用，如果 DOM 方法返回 undefined （无返回值），则 NodeList 对应方法返回 this
+ *  - 实际上可以完全使用 NodeList 来代替 DOM，不和节点关联的方法如：viewportHeight 等，在 window，document 上调用
+ *  - 存在 window/document 虚节点，通过 S.one(window)/new Node(window) ,S.one(document)/new NodeList(document) 获得
+ */
+/**
+ * definition for node and nodelist
+ * @author lifesinger@gmail.com,yiminghe@gmail.com
+ */
+KISSY.add("node/base", function(S, DOM, undefined) {
+
+    var AP = Array.prototype,
+        makeArray = S.makeArray,
+        isNodeList = DOM._isNodeList;
+
+    /**
+     * The NodeList class provides a wrapper for manipulating DOM Node.
+     * @constructor
+     */
+    function NodeList(html, props, ownerDocument) {
+        var self = this,domNode;
+
+        if (!(self instanceof NodeList)) {
+            return new NodeList(html, props, ownerDocument);
+        }
+
+        // handle NodeList(''), NodeList(null), or NodeList(undefined)
+        if (!html) {
+            return undefined;
+        }
+
+        else if (S.isString(html)) {
+            // create from html
+            domNode = DOM.create(html, props, ownerDocument);
+            // ('<p>1</p><p>2</p>') 转换为 NodeList
+            if (domNode.nodeType === DOM.DOCUMENT_FRAGMENT_NODE) { // fragment
+                AP.push.apply(this, makeArray(domNode.childNodes));
+                return undefined;
+            }
+        }
+
+        else if (S.isArray(html) || isNodeList(html)) {
+            AP.push.apply(this, makeArray(html));
+            return undefined;
+        }
+
+        else {
+            // node, document, window
+            domNode = html;
+        }
+
+        self[0] = domNode;
+        self.length = 1;
+        return undefined;
+    }
+
+    S.augment(NodeList, {
+
+        /**
+         * 默认长度为 0
+         */
+        length: 0,
+
+
+        item: function(index) {
+            var self = this;
+            if (S.isNumber(index)) {
+                if (index >= self.length) {
+                    return null;
+                } else {
+                    return new NodeList(self[index]);
+                }
+            } else {
+                return new NodeList(index);
+            }
+        },
+
+        add:function(selector, context, index) {
+            if (S.isNumber(context)) {
+                index = context;
+                context = undefined;
+            }
+            var list = NodeList.all(selector, context).getDOMNodes(),
+                ret = new NodeList(this);
+            if (index === undefined) {
+                AP.push.apply(ret, list);
+            } else {
+                var args = [index,0];
+                args.push.apply(args, list);
+                AP.splice.apply(ret, args);
+            }
+            return ret;
+        },
+
+        slice:function(start, end) {
+            return new NodeList(AP.slice.call(this, start, end));
+        },
+
+        /**
+         * Retrieves the DOMNodes.
+         */
+        getDOMNodes: function() {
+            return AP.slice.call(this);
+        },
+
+        /**
+         * Applies the given function to each Node in the NodeList.
+         * @param fn The function to apply. It receives 3 arguments: the current node instance, the node's index, and the NodeList instance
+         * @param context An optional context to apply the function with Default context is the current NodeList instance
+         */
+        each: function(fn, context) {
+            var self = this,len = self.length, i = 0, node;
+
+            for (node = new NodeList(self[0]);
+                 i < len && fn.call(context || node, node, i, self) !== false;
+                 node = new NodeList(self[++i])) {
+            }
+
+            return this;
+        },
+        /**
+         * Retrieves the DOMNode.
+         */
+        getDOMNode: function() {
+            return this[0];
+        },
+
+        /**
+         * stack sub query
+         */
+        end:function() {
+            var self = this;
+            return self.__parent || self;
+        },
+
+        all:function(selector) {
+            var ret,self = this;
+            if (self.length > 0) {
+                ret = NodeList.all(selector, self);
+            } else {
+                ret = new NodeList();
+            }
+            ret.__parent = self;
+            return ret;
+        },
+
+        one:function(selector) {
+            var self = this,all = self.all(selector),
+                ret = all.length ? all.slice(0, 1) : null;
+            if (ret) {
+                ret.__parent = self;
+            }
+            return ret;
+        }
+    });
+
+    S.mix(NodeList, {
+
+        /**
+         * enumeration of dom node type
+         */
+        ELEMENT_NODE : DOM.ELEMENT_NODE,
+        ATTRIBUTE_NODE : DOM.ATTRIBUTE_NODE,
+        TEXT_NODE:DOM.TEXT_NODE,
+        CDATA_SECTION_NODE : DOM.CDATA_SECTION_NODE,
+        ENTITY_REFERENCE_NODE: DOM.ENTITY_REFERENCE_NODE,
+        ENTITY_NODE : DOM.ENTITY_NODE,
+        PROCESSING_INSTRUCTION_NODE :DOM.PROCESSING_INSTRUCTION_NODE,
+        COMMENT_NODE : DOM.COMMENT_NODE,
+        DOCUMENT_NODE : DOM.DOCUMENT_NODE,
+        DOCUMENT_TYPE_NODE : DOM.DOCUMENT_TYPE_NODE,
+        DOCUMENT_FRAGMENT_NODE : DOM.DOCUMENT_FRAGMENT_NODE,
+        NOTATION_NODE : DOM.NOTATION_NODE,
+
+        /**
+         * 查找位于上下文中并且符合选择器定义的节点列表或根据 html 生成新节点
+         * @param {String|HTMLElement[]|NodeList} selector html 字符串或<a href='http://docs.kissyui.com/docs/html/api/core/dom/selector.html'>选择器</a>或节点列表
+         * @param {String|Array<HTMLElement>|NodeList|HTMLElement|Document} [context] 上下文定义
+         * @returns {NodeList} 节点列表对象
+         */
+        all:function(selector, context) {
+            // are we dealing with html string ?
+            // TextNode 仍需要自己 new Node
+
+            if (S.isString(selector)
+                && (selector = S.trim(selector))
+                && selector.length >= 3
+                && S.startsWith(selector, "<")
+                && S.endsWith(selector, ">")
+                ) {
+                if (context) {
+                    if (context.getDOMNode) {
+                        context = context.getDOMNode();
+                    }
+                    if (context.ownerDocument) {
+                        context = context.ownerDocument;
+                    }
+                }
+                return new NodeList(selector, undefined, context);
+            }
+            return new NodeList(DOM.query(selector, context));
+        },
+        one:function(selector, context) {
+            var all = NodeList.all(selector, context);
+            return all.length ? all.slice(0, 1) : null;
+        }
+    });
+
+    return NodeList;
+}, {
+    requires:["dom"]
+});
+
+
+/**
+ * Notes:
+ * 2011-05-25
+ *  - 承玉：参考 jquery，只有一个 NodeList 对象，Node 就是 NodeList 的别名
+ *
+ *  2010.04
+ *   - each 方法传给 fn 的 this, 在 jQuery 里指向原生对象，这样可以避免性能问题。
+ *     但从用户角度讲，this 的第一直觉是 $(this), kissy 和 yui3 保持一致，牺牲
+ *     性能，以易用为首。
+ *   - 有了 each 方法，似乎不再需要 import 所有 dom 方法，意义不大。
+ *   - dom 是低级 api, node 是中级 api, 这是分层的一个原因。还有一个原因是，如果
+ *     直接在 node 里实现 dom 方法，则不大好将 dom 的方法耦合到 nodelist 里。可
+ *     以说，技术成本会制约 api 设计。
+ */
+/**
+ * overrides methods in NodeList.prototype
+ * @author yiminghe@gmail.com
+ */
+KISSY.add("node/override", function(S, DOM, Event, NodeList) {
+
+    /**
+     * append(node ,parent) : 参数顺序反过来了
+     * appendTo(parent,node) : 才是正常
+     *
+     */
+    S.each(['append', 'prepend','before','after'], function(insertType) {
+
+        NodeList.prototype[insertType] = function(html) {
+
+            var newNode = html,self = this;
+            // 创建
+            if (S.isString(newNode)) {
+                newNode = DOM.create(newNode);
+            }
+            if (newNode) {
+                DOM[insertType](newNode, self);
+            }
+            return self;
+
+        };
+    });
+
+}, {
+    requires:["dom","event","./base","./attach"]
+});
+
+/**
+ * 2011-05-24
+ * - 承玉：
+ * - 重写 NodeList 的某些方法
+ * - 添加 one ,all ，从当前 NodeList 往下开始选择节点
+ * - 处理 append ,prepend 和 DOM 的参数实际上是反过来的
+ * - append/prepend 参数是节点时，如果当前 NodeList 数量 > 1 需要经过 clone，因为同一节点不可能被添加到多个节点中去（NodeList）
+ */KISSY.add("node", function(S, Event, Node) {
+    Node.KeyCodes = Event.KeyCodes;
+    return Node;
+}, {
+    requires:[
+        "event",
+        "node/base",
+        "node/attach",
+        "node/override",
+        "node/anim-plugin"]
+});
