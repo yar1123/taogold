fd853984c99f055b153b83cee13e76ba25ee0fe8
jayleecn@gmail.com <jayleecn@gmail.com@77b0f9dd-2c0a-c265-1d67-5e706b75b7b5>
2011-09-18 06:24:54 +0000

    优化不同状态时的提示文案

--- yzx/assets/2.0/js/main/index.js
+++ yzx/assets/2.0/js/main/index.js
@@ -21,12 +21,12 @@ KISSY.use("taogold/userlist,taogold/preview,taogold/dialog",function(S, UserList
         USE_CONFIRM_TXT = '确认启用',
         STOP_TXT = '我想禁用',
         STOP_CONFIRM_TXT = '确认禁用',
-        USING_TIPS = '上次启用操作仍在处理中，请耐心等待…',
-        STOPPING_TIPS = '上次禁用操作仍在处理中，请耐心等待…',
+        USING_TIPS = '上次启用操作仍在处理中，请耐心等待，可<a href="index.html">点此刷新</a>查看进展。',
+        STOPPING_TIPS = '上次禁用操作仍在处理中，请耐心等待，可<a href="index.html">点此刷新</a>查看进展。',
         USED_TIPS = '<b>服务已启用</b>，进入<a href="history.html">操作记录</a>可以查看哪些宝贝已成功启用了我们的服务。',
-        STOPPED_TIPS = '<b>服务未启用</b>，进入<a href="history.html">操作记录</a>可以查看最近一次的操作记录。',
-        USE_CONFIRM_TIPS = '启用服务后，系统会对您的每个宝贝进行一次全面分析，然后在其宝贝详情里自动插入相关宝贝推荐。<br/>您可以<a class="J_Preview" href="#">点此预览</a>启用效果。确定要启用吗？',
-        STOP_CONFIRM_TIPS = '禁用服务后，将从您的所有宝贝描述里删除相关宝贝推荐。<br/>确定要禁用吗？',
+        STOPPED_TIPS = '<b>服务已禁用</b>，进入<a href="history.html">操作记录</a>可以查看最近一次的操作记录。',
+        USE_CONFIRM_TIPS = '启用服务后，系统会对您的每个宝贝进行一次全面分析，然后在其宝贝详情里自动插入相关宝贝推荐。<br/>您可以<a class="J_Preview" href="#">点此预览</a>启用效果。<b>确定要启用吗？</b>',
+        STOP_CONFIRM_TIPS = '禁用服务后，将从您的所有宝贝描述里删除相关宝贝推荐。<br/><b>确定要禁用吗？</b>',
         USE_REQUEST_TIPS = '<b>服务正在启用</b>，预计 {ti} 分钟后完成，届时请按<a href="index.html">点此刷新</a>查看。<br/>您还可以<a class="J_Preview" href="#">点此预览</a>启用效果。',
         STOP_REQUEST_TIPS = '<b>服务正在禁用</b>，预计 {ti} 分钟后完成，届时请按<a href="index.html">点此刷新</a>查看。';
 
