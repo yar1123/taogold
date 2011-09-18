6be38a91f2636d9af0ae3c4ad59f2990c1fc4710
fool2fish@gmail.com <fool2fish@gmail.com@77b0f9dd-2c0a-c265-1d67-5e706b75b7b5>
2011-09-18 10:17:58 +0000

    update

--- trunk/wwg/assets/js/taogold/userlist.js
+++ trunk/wwg/assets/js/taogold/userlist.js
@@ -7,7 +7,7 @@ KISSY.add("taogold/userlist",function(S, Switchable){
         's_cap_1.gif','s_cap_2.gif','s_cap_3.gif','s_cap_4.gif','s_cap_5.gif',
         's_crown_1.gif','s_crown_2.gif','s_crown_3.gif','s_crown_4.gif','s_crown_5.gif'    
     ],
-        templ = '<div class="userinfo"><a href="http://shop{sid}.taobao.com" target="_blank">{nick}<img src="{level}" /></a> {time} 为 <b>{itemsnum}</b> 个宝贝启用了 "{tempname}" </div>';
+        templ = '<div class="userinfo"><a href="http://shop{sid}.taobao.com" target="_blank">{nick}<img src="{level}" /></a> {time} \u4e3a <b>{itemsnum}</b> \u4e2a\u5b9d\u8d1d\u542f\u7528\u4e86 "{tempname}" </div>';
     
     function UserList(container){
         var self = this;
@@ -29,7 +29,7 @@ KISSY.add("taogold/userlist",function(S, Switchable){
         },
         _render:function(data){
             var self = this, container = this.container, str = '';
-            str += '<div class="box box-padding"><div class="box-hd"><h3>最新用户</h3></div><div class="box-bd"><div class="userlist"><ul class="list">';
+            str += '<div class="box box-padding"><div class="box-hd"><h3>\u6700\u65b0\u7528\u6237</h3></div><div class="box-bd"><div class="userlist"><ul class="list">';
             for(var i = 0,len = Math.floor(data.length/3);i<len;i++){
                 str += '<li class="item">';
                 for(var j = 0; j < 3 ;j++){
