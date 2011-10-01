f9289cd104b2e2f99bcaf7c2a5a94504be7ef976
wgd243@gmail.com <wgd243@gmail.com@77b0f9dd-2c0a-c265-1d67-5e706b75b7b5>
2011-10-01 13:23:58 +0000

    add trades.get

--- trunk/topapi/increment.py
+++ trunk/topapi/increment.py
@@ -11,3 +11,27 @@ class Increment(TBase):
         self.session = sessionkey
         return self.do()
 
+    def customers_get (self, sessionkey, nick=''):
+        self.method = 'increment.customers.get'
+        if nick:
+            self.setParams(nicks=nick)
+        self.session = sessionkey
+        return self.do()
+
+    def trades_get(self, sessionkey, nick='', start='', end=''):
+        if start:
+            self.setParams(start_modified=start)
+        if end:
+            self.setParams(end_modified=end)
+        if nick:
+            self.setParams(nick=nick)
+        self.method = 'increment.trades.get'
+        self.session = sessionkey
+        return self.do()
+
+
+if __name__  == '__main__':
+    a = Increment()
+    print a.trades_get('6100a16e379b468aabf9454419c60b991534ec02b02c4da99294966', '丹丹可爱淘')
+
+
